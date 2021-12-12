f2e720dcbbadcf6178b20f47ac51de46ebb7fbd6
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-12-12 15:19:55 +0800

    修复无线mac问题

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -166,6 +166,18 @@ pushd po2lmo
 make && sudo make install
 popd
 
+# 修复无线mac问题
+rm -rf package/kernel/rtl8821cu
+svn co https://github.com/LubanCat/DoorNet-OpenWrt/trunk/package/kernel/rtl8821cu package/kernel/rtl8821cu
+svn co https://github.com/LubanCat/DoorNet-OpenWrt/trunk/target/linux/rockchip/armv8/base-files/usr/bin target/linux/rockchip/armv8/base-files/usr/bin   
+svn co https://github.com/LubanCat/DoorNet-OpenWrt/trunk/target/linux/rockchip/armv8/base-files/etc/rc.d  target/linux/rockchip/armv8/base-files/etc/rc.d
+rm -rf package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
+wget -P package/kernel/mac80211/files/lib/netifd/wireless https://raw.githubusercontent.com/DHDAXCW/RK356X/main/package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
+rm -rf package/network/services/hostapd/files/hostapd.sh
+wget -P package/network/services/hostapd/files https://raw.githubusercontent.com/DHDAXCW/RK356X/main/package/network/services/hostapd/files/hostapd.sh
+rm -rf package/kernel/mac80211/files/lib/wifi/mac80211.sh
+wget -P package/kernel/mac80211/files/lib/wifi https://raw.githubusercontent.com/DHDAXCW/RK356X/main/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
 rm -rf ./package/kernel/linux/modules/video.mk
 wget -P package/kernel/linux/modules/ https://github.com/immortalwrt/immortalwrt/raw/master/package/kernel/linux/modules/video.mk
 wget -P target/linux/rockchip/patches-5.4 https://raw.githubusercontent.com/DHDAXCW/package_target/master/107-Add-support-for-off-on-delay-kernel-5.4.patch
