0638ea20f8e65e4722933313941cedddf3bb9b7a
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-02-14 10:52:21 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -10,6 +10,7 @@ svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanag
 
 # Add luci-app-gowebdav
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
+rm -rf packages/net/gowebdav
 svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
 
 # Add tmate
@@ -20,9 +21,11 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall luci/applicati
 # git clone -b hello https://github.com/DHDAXCW/openwrt-passwall luci/applications/openwrt-passwall
 
 # Add gotop
+rm -rf packages/admin/gotop
 svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
 
 # Add minieap
+rm -rf packages/net/minieap
 svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/minieap
 
 # Replace smartdns with the official version
