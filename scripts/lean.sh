841aa32fd3636f3db4a1cb521cbd0f38041af443
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-02-21 15:29:04 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -103,7 +103,7 @@ svn co https://github.com/openwrt/packages/trunk/utils/apk
 # Add luci-udptools
 svn co https://github.com/zcy85611/Openwrt-Package/trunk/luci-udptools
 svn co https://github.com/zcy85611/Openwrt-Package/trunk/udp2raw
-svn co https://github.com/zcy85611/Openwrt-Package/trunk/udpspeeder-tunnel
+svn co https://github.com/zcy85611/Openwrt-Package/trunk/udpspeeder
 
 # Add luci-app-poweroff
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
