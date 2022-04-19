dd79580563c18f5d2fd35a8580d5c83c18863684
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-04-19 18:13:25 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -7,11 +7,6 @@ svn co https://github.com/DHDAXCW/packages/branches/ok/admin/netdata ./feeds/pac
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/MilesPoupart/openwrt-passwall ./package/lean/openwrt-passwall
 
-# Addd mosdns
-svn export https://github.com/QiuSimons/openwrt-mos/trunk/mosdns package/lean/mosdns
-svn export https://github.com/QiuSimons/openwrt-mos/trunk/luci-app-mosdns package/lean/luci-app-mosdns
-svn export https://github.com/QiuSimons/openwrt-mos/trunk/v2ray-geodata package/lean/v2ray-geodata
-
 # Add cpufreq
 rm -rf ./feeds/luci/applications/luci-app-cpufreq 
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-cpufreq ./feeds/luci/applications/luci-app-cpufreq
@@ -109,9 +104,6 @@ git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 # svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8188eu
 # svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8192eu
 # svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl88x2bu
-
-# Add luci-app-smartdns & smartdns
-svn co https://github.com/281677160/openwrt-package/trunk/luci-app-smartdns
 
 # Add luci-app-services-wolplus
 svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/luci-app-services-wolplus
