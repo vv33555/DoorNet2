af1a90bcdbd5576f6a759e64257e5b6f5db6a3c7
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-05-13 08:54:17 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -156,6 +156,7 @@ cp -f $GITHUB_WORKSPACE/scripts/patchs/994-wode.patch 994-wode.patch
 popd
 
 sed -i 's/5.15/5.4/g' target/linux/rockchip/Makefile
+
 # 删除定时coremark
 rm -rf ./customfeeds/packages/utils/coremark
 svn co https://github.com/DHDAXCW/packages/trunk/utils/coremark customfeeds/packages/utils/coremark
