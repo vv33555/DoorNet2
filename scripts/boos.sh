ea50246110854d617d2e88c9a465a89f6c3eb345
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-05-14 14:16:14 +0800

    Update boos.sh

--- scripts/boos.sh
+++ scripts/boos.sh
@@ -47,7 +47,7 @@ sed -i "s/OpenWrt /DHDAXCW @ Lubancat WRT /g" package/lean/default-settings/file
 rm -rf ./customfeeds/packages/utils/coremark
 svn co https://github.com/DHDAXCW/packages/trunk/utils/coremark customfeeds/packages/utils/coremark
 pushd target/linux/rockchip/patches-5.10
-cp -f $GITHUB_WORKSPACE/scripts/patchs/994-rockchip-rk3399-thermal-zone0-temperature-set.patch 994-rockchip-rk3399-thermal-zone0-temperature-set.patch
+cp -f $GITHUB_WORKSPACE/scripts/patch/994-rockchip-rk3399-thermal-zone0-temperature-set.patch 994-rockchip-rk3399-thermal-zone0-temperature-set.patch
 popd
 
 echo -e " DHDAXCW's FusionWrt built on "$(date +%Y.%m.%d)"\n -----------------------------------------------------" >> package/base-files/files/etc/banner
