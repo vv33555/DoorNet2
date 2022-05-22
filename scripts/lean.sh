1726837521dae45d42ea1407aa6eb81e7f1d0d70
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-05-23 07:23:07 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -9,9 +9,6 @@ sed -i 's,2016,2208,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-def
 sed -i 's,1512,1608,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
 rm -rf ./target/linux/rockchip/armv8/base-files/etc/hotplug.d/usb
 
-rm -rf package/kernel/mac80211
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/mac80211 package/kernel/mac80211
-
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
