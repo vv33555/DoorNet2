79c76da236117cd53b263c4fec2086205dd052c3
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-06-13 10:57:05 +0800

    Delete scripts/external directory

--- scripts/external/target/linux/firefly/armv8/target.mk
+++
@@ -1,8 +0,0 @@
-ARCH:=aarch64
-SUBTARGET:=armv8
-BOARDNAME:=Firefly boards(64 bit)
-
-define Target/Description
-	Build firmware image for Firefly devices.
-	This firmware features a 64 bit kernel.
-endef
