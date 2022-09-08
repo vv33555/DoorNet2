2d492575c29e630d7cff484c641a2739fcb5ceee
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-09-08 10:14:50 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -33,7 +33,7 @@ rm -rf openwrt-package/verysync
 rm -rf openwrt-package/luci-app-verysync
 
 # Add luci-app-adguardhome
-svn co https://github.com/Boos4721/OpenWrt-Packages/trunk/luci-app-adguardhome
+svn co https://github.com/Lienol/openwrt-package/branches/other/luci-app-adguardhome
 
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
@@ -62,9 +62,7 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 
 # Add ddnsto & linkease
 svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto
-svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-linkease
 svn co https://github.com/linkease/nas-packages/trunk/network/services/ddnsto
-svn co https://github.com/linkease/nas-packages/trunk/network/services/linkease
 
 # Add luci-app-onliner (need luci-app-nlbwmon)
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
